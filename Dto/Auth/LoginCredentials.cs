df7f39323734026647bd5f6b5e7ea8d98baf1b0f
Dmytro <karambolrul@gmail.com>
2021-12-09 09:49:53 +0200

    Added auth

---
+++ Dto/Auth/LoginCredentials.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkerSearchApp.Dto.Auth
+{
+    public class LoginCredentials
+    {
+        public string Login { get; set; }
+        
+        public string Password { get; set; }
+        
+        public string Role { get; set; }
+    }
+}
